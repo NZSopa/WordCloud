47faf4ac106224ef936dcf9b37b7b661448aef9e
NZSopa <sionpapa@gmail.com>
2022-02-12 22:45:22 +1300

    Word [View, Add, Delete]

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -43,17 +43,17 @@ class AppShell extends React.Component {
                     <Drawer open={this.state.toggle}>
                         <MenuItem onClick={this.handleDrawerToggle}>
                             <Link component={RouterLink} to='/'>
-                                Home
+                                홈 화면
                             </Link>
                         </MenuItem>
                         <MenuItem onClick={this.handleDrawerToggle}>
                             <Link component={RouterLink} to='/texts'>
-                                Text
+                                텍스트 관리
                             </Link>
                         </MenuItem>
                         <MenuItem onClick={this.handleDrawerToggle}>
                             <Link component={RouterLink} to='/words'>
-                                Words
+                                단어 관리
                             </Link>
                         </MenuItem>
                     </Drawer>
