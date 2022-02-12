61402ffad97b1739554a52449c087a057c2efb74
NZSopa <sionpapa@gmail.com>
2022-02-12 14:27:23 +1300

    Add Router Moudule

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import AppShell from './AppShell';
+import Home from './Home';
+import Text from './Texts';
+import Words from './Words';
 
 class App extends Component {
     render() {
         return (
-            <AppShell />
+            <Router>
+                <AppShell >
+                    <div>
+                        <Routes>
+                            <Route exact path='/' element={<Home />} />
+                            <Route exact path='/Texts' element={<Text />} />
+                            <Route exact path='/Words' element={<Words />} />
+                        </Routes>
+                    </div>
+                </AppShell>
+            </Router>
         );
     }
 }
--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Drawer from '@material-ui/core/Drawer';
@@ -12,6 +14,10 @@ const styles = {
     },
     menuButton: {
         marginRight: 'auto'
+    },
+    content: {
+        margin: 'auto',
+        marginTop: '20px'
     }
 }
 class AppShell extends React.Component {
@@ -27,15 +33,34 @@ class AppShell extends React.Component {
     render() {
         const { classes } = this.props;
         return (
-            <div className={classes.root}>
-                <AppBar position="static">
-                    <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
-                        <MenuIcon />
-                    </IconButton>
-                </AppBar>
-                <Drawer open={this.state.toggle}>
-                    <MenuItem onClick={this.handleDrawerToggle}>Home</MenuItem>
-                </Drawer>
+            <div>
+                <div className={classes.root}>
+                    <AppBar position="static">
+                        <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
+                            <MenuIcon />
+                        </IconButton>
+                    </AppBar>
+                    <Drawer open={this.state.toggle}>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to='/'>
+                                Home
+                            </Link>
+                        </MenuItem>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to='/texts'>
+                                Text
+                            </Link>
+                        </MenuItem>
+                        <MenuItem onClick={this.handleDrawerToggle}>
+                            <Link component={RouterLink} to='/words'>
+                                Words
+                            </Link>
+                        </MenuItem>
+                    </Drawer>
+                </div>
+                <div id="content" className={classes.content}>
+                    {React.cloneElement(this.props.children)}
+                </div>
             </div>
         )
     }
